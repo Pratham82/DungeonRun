7e14123ea84282cc8d97d71c06bad0353fcf7e23
Pratham82 <mali.prathamesh82@gmail.com>
2019-12-31 12:54:59 +0530

    Files for Dungeon run

---
+++ ProjectFiles/Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour {
+    private Renderer rend;
+    // Use this for initialization
+    [SerializeField]
+    private Color colorToTurnTo = Color.white;
+	private void Start () {
+        rend = GetComponent<Renderer>();
+        rend.material.color = colorToTurnTo;
+    }
+}
